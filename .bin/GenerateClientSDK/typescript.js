20551547bec05e13c58e787a09a67dc7117b40bf
Gabriel C. Troia <catalin.troia@gmail.com>
2017-09-10 14:07:42 -0400

    Actually not the best idea, b/c every client has to use the same env as me to build

---
+++ .bin/GenerateClientSDK/typescript.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var util_1 = require("../util");
+var Promise = require("bluebird");
+var R = require("ramda");
+var compileTpl = R.curry(function (matchers, content) {
+    return R.reduce(function (result, toFind) {
+        var replaceWith = matchers[toFind];
+        var regex = new RegExp(toFind, 'g');
+        return result.replace(regex, replaceWith);
+    }, content, R.keys(matchers));
+});
+exports.generate = function (appName) {
+    var dirPath = __dirname + '/../../SDKTemplates/typescript';
+    var compile = compileTpl({
+        '{{=APP_NAME}}': appName,
+    });
+    return Promise.all([
+        util_1.readFile(dirPath + '/typescript.d.ts.tpl').then(compile),
+        util_1.readFile(dirPath + '/typescript.js.tpl').then(compile),
+    ]);
+};
+//# sourceMappingURL=typescript.js.map
---
+++ .bin/Version/detectChanges.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var R = require("ramda");
+var deep_diff_1 = require("deep-diff");
+var changeTypes = {
+    E: 'patch',
+    N: 'minor',
+    D: 'major',
+};
+var changeTypePriorities = {
+    NONE: 0,
+    E: 10,
+    N: 20,
+    D: 30,
+};
+var getChangesKind = R.reduce(function (prevKind, _a) {
+    var kind = _a.kind;
+    return ((changeTypePriorities[kind] > changeTypePriorities[prevKind]) ? kind : prevKind);
+}, 'NONE');
+exports.detectChanges = function (a, b) {
+    return changeTypes[getChangesKind(deep_diff_1.diff(a, b) || [])];
+};
+//# sourceMappingURL=detectChanges.js.map
